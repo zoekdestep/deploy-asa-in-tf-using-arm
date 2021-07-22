bb859a0248bfef8b7952b675bc791c68ae6f73c0
Jan C. Schäfer <jan@schae.fr>
2021-07-22 09:07:47 +0200

    Adding Pipefail
    Check https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425

--- state-resources.sh
+++ state-resources.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Ensure safe execution
+set -euxo pipefail
+
 # Log into an Azure tenant using a service principal
 az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 
